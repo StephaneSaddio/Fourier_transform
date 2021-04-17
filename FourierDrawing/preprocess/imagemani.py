0079963a9826ef9da6999b325ad8ca2bca220015
KamalAyoubi <78489300+KamalAyoubi@users.noreply.github.com>
2021-04-17 19:15:39 +0200

    add convert_binary function

--- FourierDrawing/preprocess/imagemani.py
+++ FourierDrawing/preprocess/imagemani.py
@@ -34,10 +34,30 @@ class Imagemanip:
         # convert image to single color 
         self.img_single_color = self.img.convert('L')
 
+    def Binarize(self, scale=3, thresh_val=200):   
+        
+        # convert image to nympy array 
+        image_array = np.array(image)
+
+        # Binarize a numpy array using thresh_val as cutoff
+        for i in range(len(image_array)):
+            for j in range(len(image_array[0])):
+                if image_array[i][j] > thresh_val:
+                    image_array[i][j] = 255
+                else:
+                    image_array[i][j] = 0
+        
+        image = Image.fromarray(image_array)
+        
+        # scale image down to reduce number of non-zero pixels
+        self.img_sm = image.resize(tuple([int(v/scale) for v in image.size]),Image.ANTIALIAS)
+    
+
     def black_and_white(self):
         
         # convert image to black and white
         self.img_blackwhite = self.img.convert(mode='1', dither=2)
+
 
 #%%
 
