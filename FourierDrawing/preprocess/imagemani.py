5fba7aff18ae5526e367431795f62cee407edb87
KamalAyoubi <78489300+KamalAyoubi@users.noreply.github.com>
2021-04-17 15:58:45 +0200

    import image update instruction

--- FourierDrawing/preprocess/imagemani.py
+++ FourierDrawing/preprocess/imagemani.py
@@ -1,24 +1,15 @@
-
 #%%
 from PIL import Image
 import requests
 from io import BytesIO
 import pylab
+import numpy as np
+
 
 #%%
 url = 'https://www.seekpng.com/png/detail/116-1164659_line-drawing-bunny-rabbit-at-getdrawings-bunny-drawing.png'
 response = requests.get(url)
-img = Image.open(BytesIO(response.content))
-pylab.imshow(img)
+img_brut = Image.open(BytesIO(response.content))
+pylab.imshow(img_brut )
 pylab.show()
 
-
-# %%
-
-img = img.convert('L')
-fig, ax = pylab.subplots()
-contours = ax.contour(img, origin='image', levels=[100])
-pylab.show()
-
-
-# %%
