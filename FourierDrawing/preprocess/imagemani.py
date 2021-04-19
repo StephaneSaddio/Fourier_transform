7a8965db20622f6037ff5dc2849861af756afe7f
KamalAyoubi <78489300+KamalAyoubi@users.noreply.github.com>
2021-04-19 03:27:27 +0200

    add countours function & updates

--- FourierDrawing/preprocess/imagemani.py
+++ FourierDrawing/preprocess/imagemani.py
@@ -5,7 +5,8 @@ from io import BytesIO
 import pylab
 import numpy as np
 
-
+from copy import deepcopy
+from scipy.spatial import distance
 #%%
 url = 'https://www.seekpng.com/png/detail/116-1164659_line-drawing-bunny-rabbit-at-getdrawings-bunny-drawing.png'
 #url = 'https://lh3.googleusercontent.com/proxy/_j7J9PlqYfCUUFduzzKbulOkZvdr526F88U481R5tV0eZGlNU2mNr-fURkUBseryy3aUIuc_x2uycYPcwE6QnQG05qdQ3E_5nEvlD0MF5M6zELEZa4CHIUjufmw-s_LoIdJO-Pk'
@@ -48,12 +49,13 @@ class Imagemanip:
         image_array = np.array(self.img_single_color)
 
         # convert to binary image_array using thresh_val to cut
-        white = 255
-        black = 0
+        for i in range(len(image_array)):
+            for j in range(len(image_array[0])):
+                if image_array[i][j] > thresh_val:
+                    image_array[i][j] = 255
+                else:
+                    image_array[i][j] = 0
 
-        initial_conv = np.where((image_array <= 200), image_array, white)
-        final_conv = np.where((initial_conv > 200), initial_conv, black)
-           
         image = Image.fromarray(image_array)
         
         # reduce number of non-zero pixels by scaling down the image
@@ -62,7 +64,7 @@ class Imagemanip:
     def black_and_white(self):
         
         # convert image to black and white
-        self.img_blackwhite = self.img.convert(mode='1', dither=2)
+        self.img_blackwhite = self.img_scale.convert(mode='1', dither=2)
         self.pixels = (1 - np.asarray(self.img_blackwhite).astype(int))
         self.pixels_line = np.reshape(self.pixels, self.pixels.size)
 
@@ -78,6 +80,26 @@ class Imagemanip:
 
     def distance_matrix(self):
     
+        ## Get positions of non-zero pixels
+        #absolute_index = np.where(self.pixels_line > 0)[0] 
+        #relative_index = np.array(range(1, len(absolute_index)+1 ))
+        #
+        ## Replace each non-zero pixel in the array with its number
+        ## i.e., the 10th non-zero pixel will have 10 in its place
+        #self.flat_img_mod = deepcopy(self.pixels_line)
+        #for rel, pix in enumerate(absolute_index):
+        #    self.flat_img_mod[pix] = rel+1
+#
+        ## Get coordiantes for each non-zero pixel
+        #img_idx = np.reshape(self.flat_img_mod, self.pixels.shape)
+        #self.coord_list = []
+        #for p1 in relative_index:
+        #    p1_coords = tuple([int(c) for c in np.where(img_idx==p1)])
+        #    self.coord_list.append(list(p1_coords))
+        #
+        ## Calcualte distance between each pair of coords
+        #self.dist_mat = distance.cdist(self.coord_list, self.coord_list, 'euclidean')   
+
         # Find positions of non-zero pixels
         non_zero_P_index = np.where(self.pixels_line > 0)[0]
         # Make a range array index form 1 to len(non_zero_P_index)
@@ -98,7 +120,44 @@ class Imagemanip:
         # Calcualte distance between each pair of coords
         self.distance_matrix = distance.cdist(self.coord_list, self.coord_list, 'euclidean')
 
+   
+    def contours_search(self, plot=True):
+
+        # Initialize search space with nearest neighbor tour
+
+        edges = self.coord_list
+        length_edges = len(edges)
+        #....
+        start = 1300
+        # ...
+        tour = [start]
+        current_edge = start
+        # ... 
+        for step in range(0, length_edges):
+            dist_row = deepcopy(self.distance_matrix[current_edge,:])
+            for k in tour:
+                dist_row[k] = np.inf
+            nearest_neighbor = np.argmin(dist_row)
+            if nearest_neighbor not in tour:
+                tour.append(nearest_neighbor)
+            current_edge = nearest_neighbor
+
+        y_tour = -np.array([edges[tour[i % length_edges]] for i in range(length_edges+1) ])[:,0]
+        y_tour = y_tour - y_tour[0]#- min(y_tour)
+        x_tour = np.array([edges[tour[i % length_edges]] for i in range(length_edges+1) ])[:,1]    
+        x_tour = x_tour - x_tour[0]#- min(x_tour)
+
+        # Circle tour back to beginning
+        np.append(x_tour, x_tour[0])
+        np.append(y_tour, y_tour[0])
+        length_edges = length_edges + 1
     
+        self.x_tour = x_tour
+        self.y_tour = y_tour
+        self.length_pixels = length_edges
+
+        if plot:
+            plt.plot(self.x_tour, self.y_tour)
 #%%
 rabbit = Imagemanip(img_raw)
 rabbit.show()
@@ -108,7 +167,13 @@ rabbit.show()
 rabbit.single_color()
 rabbit.convert_binary(scale=3, thresh_val=200)
 rabbit.black_and_white()
+
 rabbit.show_black_and_white()
 # %%
 rabbit.distance_matrix()
+
+#%%
+rabbit.contours_search(plot=True)
+# %%
+
 # %%
