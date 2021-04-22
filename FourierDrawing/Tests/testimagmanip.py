c26bb0d950be0460df99df7ac5d463ca1aa370ab
KamalAyoubi <78489300+KamalAyoubi@users.noreply.github.com>
2021-04-22 16:05:22 +0200

    testing Imgmanip class

---
+++ FourierDrawing/Tests/testimagmanip.py
@@ -0,0 +1,26 @@
+import sys
+import unittest
+
+sys.path.append('..')
+from ImageManipulation.Imgmanip import Imagemanip
+
+url = 'https://www.seekpng.com/png/detail/116-1164659_line-drawing-bunny-rabbit-at-getdrawings-bunny-drawing.png'
+
+class test_Imgmanip(unittest.TestCase):
+
+    def test_img_object (self):
+        img = Imagemanip(url)
+        self.assertIsInstance(img, Imagemanip)
+
+    def test_thresh_val (self):
+        img = Imagemanip(url)
+        img.single_color()
+        img.convert_binary(scale=3, thresh_val=200)
+        self.assertGreater(img.thresh_val, 0) # thresh_val > 0
+        self.assertLess(img.thresh_val, 255) # thresh_val < 255
+
+
+
+if __name__ == '__main__':
+    unittest.main()
+
