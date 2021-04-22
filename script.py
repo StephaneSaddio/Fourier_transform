5e01d8b769dc0c4187a44d9b16b777e5b3780361
KamalAyoubi <78489300+KamalAyoubi@users.noreply.github.com>
2021-04-22 19:55:48 +0200

    update

---
+++ script.py
@@ -0,0 +1,7 @@
+
+import sys
+sys.path.append('..')
+from FourierDrawing.ImageManipulation.Imgmanip import Imagemanip
+
+url = 'https://www.seekpng.com/png/detail/116-1164659_line-drawing-bunny-rabbit-at-getdrawings-bunny-drawing.png'
+Imagemanip(url)
