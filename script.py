44dcd7c7fdc71f48c6bcc518f0a6e31053135be2
KamalAyoubi <78489300+KamalAyoubi@users.noreply.github.com>
2021-04-22 20:12:36 +0200

    update

--- script.py
+++ script.py
@@ -1,7 +1,26 @@
-
-import sys
-sys.path.append('..')
 from FourierDrawing.ImageManipulation.Imgmanip import Imagemanip
 
 url = 'https://www.seekpng.com/png/detail/116-1164659_line-drawing-bunny-rabbit-at-getdrawings-bunny-drawing.png'
-Imagemanip(url)
+img = Imagemanip(url)
+img.show()
+
+rabbit = Imagemanip(url)
+rabbit.show()
+
+
+
+rabbit.single_color()
+
+rabbit.convert_binary(scale=3, thresh_val=200)
+
+rabbit.thresh_val
+
+rabbit.black_and_white()
+
+rabbit.show_black_and_white()
+
+rabbit.distance_matrix()
+
+rabbit.contours_search(plot=True)
+
+rabbit.get_splines(plot=True)
