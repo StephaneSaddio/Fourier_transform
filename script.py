c65b11ffc8f3afbb8cfcf88723d0594d563ad4c5
Stéphane SADIO <stephane.sadio@etu.umontpellier.fr>
2021-04-24 03:18:38 +0200

    script update

--- script.py
+++ script.py
@@ -1,5 +1,35 @@
-import FourierDrawing_brd
+#%%
+from FourierDrawing.ImageManipulation.Imgmanip import Imagemanip
+from FourierDrawing.FourierApproximation.Class_fourierApproximation import FourierApprox
+from FourierDrawing.Circles_radii_center.ComplexCircles import Circles
+#from Animate_FT import Animate 
+#%%
 
-im_Image = FourierDrawing_brd.image().save_as_df()
-df_nicely_formated = biketrauma_brd.format_date(df)
-biketrauma_brd.plot_location(biketrauma_brd.get_accident(df))
+url = 'https://www.seekpng.com/png/detail/116-1164659_line-drawing-bunny-rabbit-at-getdrawings-bunny-drawing.png'
+
+
+img = Imagemanip(url)
+img.show()
+
+
+#%%
+img.single_color()
+img.convert_binary(scale=3, thresh_val=200)
+img.black_and_white()
+
+img.show_black_and_white()
+# %%
+img.distance_matrix()
+
+#%%
+img.contours_search(plot=True)
+# %%
+
+img.get_splines(plot=True)
+#%%
+ani = animation.FuncAnimation(fig, animate, frames=num_frames,
+                              interval=interval, blit=True, init_func=init)
+#%%
+
+ani.save('./Images/animation5.gif', writer='imagemagick')
+DisplayImage(url='./Images/animation5.gif')
